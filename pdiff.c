e573cb14442e34f878a38803dbd3cb6a9d61a4e4
Raycho Mukelov <raycho.mukelov@pcloud.com>
2019-09-13 19:22:06 +0300

    Revert "fix"
    This reverts commit 9782b398afbfe9328856707f369bcc34ac0b6c19.

--- pdiff.c
+++ pdiff.c
@@ -1822,7 +1822,7 @@ static void process_acceptedshareout(const binresult *entry){
   }
 }
 
-static void process_establishbshareout(const binresult *entry){
+static void process_establishbshareout(const binresult *entry) {
   psync_sql_res *q;
   const binresult *share, *br, *ownid, *fromuserid;
   char *email = 0;
@@ -1831,12 +1831,13 @@ static void process_establishbshareout(const binresult *entry){
 
   if (!entry)
     return;
+
   share=psync_find_result(entry, "share", PARAM_HASH);
   ownid=psync_check_result(share, "folderownerid", PARAM_NUM);
   if(ownid){
     folderowneruserid = ownid->num;
     psync_get_current_userid(&owneruserid);
-    fromuserid=psync_check_result(share, "fromuserid", PARAM_NUM)->num;
+    fromuserid=psync_check_result(share, "fromuserid", PARAM_NUM);
     if (fromuserid&&fromuserid->num==owneruserid){
       isincomming=0;
     }
